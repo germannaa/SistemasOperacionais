e44f1b7f5816d06ae2ecc1ba29c22309320b9ab3
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-31 19:49:58 -0300

    Create Servidor.py

---
+++ Servidor.py
@@ -0,0 +1,42 @@
+'''import socket 
+host = 'localhost' 
+port = 7000 
+addr = (host, port) 
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+serv_socket.bind(addr) 
+serv_socket.listen(10) 
+print ('aguardando conexao') 
+con, cliente = serv_socket.accept() 
+print ('conectado' )
+print ("aguardando mensagem") 
+recebe = con.recv(1024) 
+print ('mensagem recebida:'+ recebe )
+serv_socket.close()
+'''
+
+
+import socket
+ 
+HOST = '127.0.0.1 '     # Endereco IP do Servidor
+PORT = 2345      # Porta que o Servidor está
+ 
+# Criando a conexão
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+destino = (HOST, PORT)
+tcp.connect(destino)
+ 
+print('\nDigite suas mensagens')
+print('Para sair use CTRL+X\n')
+ 
+# Recebendo a mensagem do usuário final pelo teclado
+mensagem = input()
+ 
+# Enviando a mensagem para o Servidor TCP através da conexão
+while mensagem != '\x18':
+    tcp.send(str(mensagem).encode())
+    mensagem = input()
+ 
+# Fechando o Socket
+
+    tcp.close()
