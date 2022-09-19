caa96b7054dd134710b9df34d882b5058b2225b1
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-19 19:42:55 -0300

    Create Corrida.java

---
+++ CorridaThread/Corrida.java
@@ -0,0 +1,31 @@
+
+package newpackage;
+
+
+public class Corrida extends Thread{
+    String nome;
+    int trajeto;
+    int descanso;
+    
+    public Corrida(String nome, int trajeto, int descanso){
+        this.nome = nome;
+        this.trajeto=trajeto;
+        this.descanso=descanso;
+    }
+
+@Override
+public void run(){
+    for(int i = trajeto; i > 0; i--){
+        System.out.println(String.format("Faltam %d para o %s", i, nome));
+        
+    try {
+        System.out.printf("%s está descansando", nome);
+        sleep(descanso*1000);
+    } catch (InterruptedException e){
+            e.printStackTrace();
+    }
+}
+    
+    System.out.printf("%s ----- Chegou!!", nome);
+}
+}
